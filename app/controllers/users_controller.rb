f144f90af65008e8c985bea2a46bffdf72071da5
JordanJoseph <jordant.joseph@outlook.com>
2022-10-31 22:11:34 -0600

    add routes for user create and show actions, define in users_controller along with user_params, add validators to user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+
+    def show
+        current_user = User.find(session[:user_id])
+        render json: current_user, status: :ok
+    end 
+
+    def create
+        user = User.create(user_params)
+        if user.valid? 
+            render json: user, status: :created
+        else
+            render json: {errors: user.errors.full_messages}, status: :unprocessable_entity
+        end
+    end
+
+    private 
+
+    def user_params
+        params.permit(:username, :password)
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+
+    validates :username, presence: true, uniqueness: true
+    validates :password, presence: true, length: {in: 3..20}
+    
 end
