a8f68792099db62fe7c2dd50451ffa0df20ed194
JordanJoseph <jordant.joseph@outlook.com>
2022-10-31 22:46:09 -0600

    create login component, add route in App and link on Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
+import Login from './components/Login';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Navbar /> 
       <Routes>
         <Route path='/' element={<Home/>} />
+        <Route path='/login' element={<Login/>} />
       </Routes>
     </Router>
   );
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Navbar = () => {
 
     return(
-        <p>Navbar !</p>
+        <>
+            <p>Navbar !</p>
+            <button component={Link} to='/login'>
+                Login
+            </button>
+        </>
     )
 }
 
