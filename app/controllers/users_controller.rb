14c041811c204ed622f706ca632aa33061ce6e8a
JordanJoseph <jordant.joseph@outlook.com>
2022-11-01 14:31:59 -0600

    remove user from user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
     private 
 
     def user_params
-        params.permit(:username, :password, :user)
+        params.permit(:username, :password)
     end
 
 end
