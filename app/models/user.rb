f0aa9d9d4d3229f19a01f23440256829f11cb5ed
JordanJoseph <jordant.joseph@outlook.com>
2022-11-09 14:15:23 -0700

    add books to user_serializer, distinct in the User model to remove duplicate book titles, list reviewed books in UserProfile

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
     has_many :reviews
-    has_many :books, through: :reviews 
+    has_many :books, -> { distinct }, through: :reviews 
+
+    #{scope :favorite, -> { where(favorite: true) } }
+    #scope :filter_by_favorite, -> (favorite) { where favorite: true }
     
     has_secure_password 
     validates :username, presence: true, uniqueness: true
--- client/src/components/UserProfile.jsx
+++ client/src/components/UserProfile.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import Login from './Login';
+import Box from '@mui/material/Box';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+import Button from '@mui/material/Button';
 
 const UserProfile = ({user}) => {
+
+    user.books.map(book => console.log(book.title))
 
     if (!user) {
         return(
@@ -10,10 +17,36 @@ const UserProfile = ({user}) => {
     } else{
         return(
             <div class='center'>
-                <h1>Profile</h1>
-                <ul>Username: {user.username}</ul>
-                <p>favorited books in here !</p>
                 
+                <Box sx={{ 
+                    position: 'absolute', 
+                    left: '50%', 
+                    top: '55%',
+                    transform: 'translate(-50%, -50%)', 
+                    width: 950, 
+                    height: 650 }}
+                >
+                <h1>{user.username}'s Favorite Books</h1>
+                <ImageList variant="masonry" cols={3} gap={8}> 
+                {user.books.map((book) => {
+                    return(
+                        <ImageListItem key={book.id}>
+                        <img
+                          src={`${book.image}`}
+                          srcSet={`${book.image}`}
+                          alt={book.title}
+                          loading="lazy"
+                        />
+                        <ImageListItemBar
+                          title={book.title}
+                          subtitle={<span>by: {book.author}</span>}
+                          position="below"
+                        />
+                      </ImageListItem>
+                    )
+                })}
+                </ImageList>
+                </Box> 
             </div>
         )
     }
