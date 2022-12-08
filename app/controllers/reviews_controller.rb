c23ab1a3372063ec7e0e347b914f80e7b71bf377
JordanJoseph <jordant.joseph@outlook.com>
2022-12-08 16:52:30 -0700

    create private methods for finding reviews and not_found response, refactor code and add rescue_from

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,7 @@
 class ReviewsController < ApplicationController
 
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
     def create
         review = Review.create(review_params)
         if review.valid?
@@ -10,23 +12,15 @@ class ReviewsController < ApplicationController
     end 
 
     def update 
-        review = Review.find_by(id: params[:id])
-        if review 
-            review.update(review_params)
-            render json: review, status: :accepted 
-        else 
-            render json: {error: "review not found"}, status: :not_found
-        end
+        review = find_review
+        review.update(review_params)
+        render json: review, status: :accepted 
     end
 
     def destroy
-        review = Review.find_by(id: params[:id])
-        if review 
-            review.destroy 
-            head :no_content 
-        else 
-            render json: {error: "review not found"}, status: :not_found
-        end
+        review = find_review
+        review.destroy 
+        head :no_content 
     end
 
     private 
@@ -34,5 +28,13 @@ class ReviewsController < ApplicationController
     def review_params
         params.permit(:id, :user_id, :book_id, :comment, :favorite)
     end
+
+    def find_review
+        review = Review.find(params[:id])
+    end 
+
+    def render_not_found_response
+        render json: {error: "review not found"}, status: :not_found
+    end 
 
 end
